0a40bb0961d8e089517fa86239078a6574030141
John Lawson <lawsonjohnr@gmail.com>
2026-07-16 16:52:48 -0500

    Added NavLinks to NavBar

--- src/Layout/NavBar.jsx
+++ src/Layout/NavBar.jsx
@@ -1,10 +1,17 @@
+import {NavLink} from "react-router"
+
 const NavBar = () => {
     return(
         <nav>
             <h1> Hi John</h1>
-            <a>Home</a>
-            <a>Account</a>
-            <a>Login</a>
+            <NavLink to="/">Home</NavLink>
+            {UserActivation.id ? (<span>
+            <NavLink to="/ProfilePage">Profile</NavLink>
+
+            </span>) : (<span>
+            <NavLink to="/Login">Login</NavLink>
+            <NavLink to="/Register">Register</NavLink>
+            </span>)}
         </nav>
         
     )
